6f7ce73be9c57b3a0170abc8d859666ed188fc33
MUHAMMAD-FATUR-SOFYAN-KUSUMAH <mfatur.fs@gmail.com>
2025-01-13 20:07:34 +0700

    testing github ssh

--- bottomchannleing.go
+++ bottomchannleing.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	ch1 := make(chan int)
-	ch2 := make(chan int)
+	ch2_add_change := make(chan int)
 
 	// Goroutine untuk mengirim data ke ch1
 	go func() {
@@ -15,10 +15,10 @@ func main() {
 		ch1 <- 1
 	}()
 
-	// Goroutine untuk mengirim data ke ch2
+	// Goroutine untuk mengirim data ke ch2_add_change
 	go func() {
 		time.Sleep(2 * time.Second)
-		ch2 <- 2
+		ch2_add_change <- 2
 	}()
 
 	for i := 0; i < 2; i++ {
@@ -26,11 +26,11 @@ func main() {
 		select {
 		case value := <-ch1:
 			fmt.Println("Received from ch1:", value)
-		case value := <-ch2:
-			fmt.Println("Received from ch2:", value)
+		case value := <-ch2_add_change:
+			fmt.Println("Received from ch2_add_change:", value)
 		}
 	}
 	// Output:
 	// Received from ch1: 1
-	// Received from ch2: 2
+	// Received from ch2_add_change: 2
 }
